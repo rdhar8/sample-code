0d5122bc896cbb092c6b369d079580ff1afc5ea9
rajanutanix <rajanutanix@users.noreply.github.com>
2023-04-11 10:43:46 +0000

    Automated eslint fixes

--- test.js
+++ test.js
@@ -3,15 +3,20 @@ console.log('Hello World! Test');
 
 console.log('Hello World!');
 
-if(true) console.log('here');
+if(true) {
+  console.log('here');
+}
 
 const autofix_name = 10;
 
-if  (true)
-      console.log('here');
+if (true) {
+  console.log('here');
+}
 
-function autofixname1() {
-  if(true) return true;
+function autofixname1 () {
+  if(true) {
+    return true;
+  }
   return false;
 }
 
