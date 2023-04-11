58477151cafc1a020e13bce5073d434943ecfc32
Raja <raja.madhu@nutanix.com>
2023-04-11 15:48:55 +0530

    test more rules

--- test.js
+++ test.js
@@ -1,4 +1,18 @@
 
 console.log('Hello World! Test');
 
-console.log('Hello World!')
+console.log('Hello World!');
+
+if(true) console.log('here');
+
+const autofix_name = 10;
+
+if  (true)
+      console.log('here');
+
+function autofixname1() {
+  if(true) return true;
+  return false;
+}
+
+let x = 10;
