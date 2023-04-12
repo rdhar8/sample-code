ccd68924b368302e1aff6aa9432f12ad48b6d5ae
rdhar8 <riya.dhar@nutanix.com>
2023-04-12 15:36:41 +0530

    very important changes
    changes

---
+++ AnotherVeryImportantComponent.js
@@ -0,0 +1,11 @@
+function AnotherVeryImportantComponent () {
+  console.log('Go to Next Step');
+
+  return (
+    <div style={{ 'display': 'flex' }}>
+      <a href={'dummy link'}>Sample Text</a>
+    </div>
+  );
+}
+
+export default AnotherVeryImportantComponent;
