1909085d3659ac34d7bc447c563f5844cdf3b32c
pratikjagtap-nutanix <pratikjagtap-nutanix@users.noreply.github.com>
2023-04-12 06:08:15 +0000

    Automated eslint fixes

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 
 console.log('Hello World! Test');
 
-console.log('Hello World!')
+console.log('Hello World!');
 
-console.log('Hello Coder')
+console.log('Hello Coder');
 
 
-console.log('Hello World!!!')
+console.log('Hello World!!!');
