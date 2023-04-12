b1cb134514ee14d9d2853fc21fd097e079011710
rajanutanix <raja.madhu@nutanix.com>
2023-04-12 12:11:53 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,25 @@
+import numpy as np
+import time
+import pandas as pd
+
+Captain='Picard'
+
+def InitiateWarpSpeed(order):
+    if order=="engage":
+        print("initiating warp speed")
+    else:
+        print("you are not the captain of this vessel")
+
+InitiateWarpSpeed("engage")
+
+
+def is_number_even(num):
+    return "Even" if num % 2 == 0 else "Odd"
+
+num = 5
+print(f"The number {num} is {is_number_even(num)}")
+
+
+print "Hello"
+  print "Hello2"
+
